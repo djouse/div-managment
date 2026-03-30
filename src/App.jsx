7e11d9778cacee1c0fd7b9a67d0510e10d7eabb4
José Dias <jdptdias@outlook.pt>
2026-03-30 23:58:10 +0100

    Init sandbox - div management exercises

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+function App() {
+
+  return (
+    <div className="flex gap-5">
+      <div className="w-20 h-20 bg-red-500 flex items-center">
+        1
+      </div>
+      <div className="w-20 h-20  bg-red-500">
+        2
+      </div>
+      <div className="w-20 h-20 bg-red-500">
+        3
+      </div>
+      <div className="w-20 h-20 bg-red-500">
+        4
+      </div>
+      <div className="w-20 h-20 bg-red-500">
+        5
+      </div>
+      <div className="w-20 h-20 bg-red-500">
+        6
+      </div>
+      <div className="w-20 h-20 bg-red-500">
+        7
+      </div>
+      <div className="w-20 h-20 bg-red-500">
+        8
+      </div>
+    </div>
+  )
+}
+
+export default App
